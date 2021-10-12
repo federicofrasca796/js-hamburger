096d81e1485c8d318e135435d22f07ee44362fc4
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-12 15:43:11 +0200

    fix bug: hamburger-menu not displaying properly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
 //select DOM .hamburger-menu element
 const elementMenu = document.querySelector(".hamburger-menu");
-// console.log(elementMenu)
+    // console.log(elementMenu)
 
 //select DOM menu icon 
 const menuBarsIcon = document.querySelector(".fa-bars");
-console.log(menuBarsIcon)
+    // console.log(menuBarsIcon)
 
-//on ICON click activate .active class in div.hamburger-menu
+//select DOM closeMenu icon
+const closeMenuIcon = document.querySelector(".close")
+    // console.log(closeMenuIcon)
+
+
+//on ICON click, activate .active class in div.hamburger-menu
 menuBarsIcon.addEventListener("click", function(){
-    elementMenu.className += "active";
+    elementMenu.className += " active";
 });
+
+//on CLOSE ICON click, remove .active class from div.hamburger-menu 
+// closeMenuIcon.addEventListener("click", function(){
+//     elementMenu.className -= " active";
+//     console.log(elementMenu)
+
+// })
