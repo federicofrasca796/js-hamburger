46d062b5fd1bedeaa34c85e3f2041ccaf96157cd
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-12 15:11:34 +0200

    add initial logic|initialize const w/ DOM element
    fix links in html

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//select DOM element
+const elementMenu = document.querySelector(".hamburger-menu")
+console.log(elementMenu)
+
+//on click activate .active class in div.hamburger-menu
+document.addEventListener("click", function(){
+    //here    
+});
